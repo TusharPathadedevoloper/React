47ec3eb43abffae7d4bd00088b9c1b8c686a055d
TusharPathadedevoloper <tusharpathade32@gmail.com>
2023-10-16 14:47:58 +0530

    first commit

---
+++ project-1/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <div className='logo'>
+          <img src="/images/logonike.jpeg" alt="nike logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">Rate Us</li>
+          <li href="#">Contact Us</li>
+        </ul>
+        <button>Log In</button>
+      </nav>
+
+    </div>
+  )
+}
+
+export default App ;
---
+++ project-1/src/Components/Shoes.jsx
@@ -0,0 +1,22 @@
+
+
+const Shoes = () => {
+  return (
+    <main className="shoes">
+      <div>
+        <h1>Run Your Run</h1>
+       
+        <p>Follow the feeling that keeps you running  your best in the city</p>
+      <div className="shoes-button">
+        <button>Shop Now</button>
+        <button className="sec-button">Category</button>
+      </div>
+      </div>
+      <div className="nike-image">
+        <img src="images/l.webp" alt="" />
+      </div>
+    </main>
+  )
+}
+
+export default Shoes ;
